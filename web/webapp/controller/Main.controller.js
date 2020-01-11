20d3fad01e7fd17a4d0d04a0fd9725605ef44f39
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2020-01-11 17:22:40 +0000

    Adição do modulo web.

---
+++ web/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("jsg.fix-preco-liquido.web.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ web/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"jsg/fix-preco-liquido/web/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
