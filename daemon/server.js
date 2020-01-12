82efafce2f96836d98ce403249ca1f97926af1c2
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2020-01-12 18:35:46 +0000

    Obtenção de informações no tipo de serviço destination no daemon App. Adição da App router,

--- daemon/server.js
+++ daemon/server.js
@@ -3,10 +3,27 @@
 
 var http = require("http");
 var port = process.env.PORT || 3000;
+var destination = require("./destination");
+const destinationService = "dest_FixLiquido";
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World\n");
+http.createServer( function (req, res) {
+  
+	Promise.all([
+		destination.getDestination(destinationService, "s4hc"), 
+		destination.getDestination(destinationService, "taxService")]
+		)
+		.then( destinations => {
+  			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.end(JSON.stringify(destinations));
+		});
+	
+  /*
+  res.end("Hello World\n"+JSON.stringify({
+            clientid: destination.clientid,
+            clientsecret: destination.clientsecret,
+            url: destination.url
+        }));
+  */
 }).listen(port);
 
 console.log("Server listening on port %d", port);
