055b463e15aacfea16cb43790a0a0e21e8ed087d
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2020-02-07 18:25:52 -0300

    Adição da ativação e desativação do daemon.

--- router/resources/ui5/webapp/controller/Home.controller.js
+++ router/resources/ui5/webapp/controller/Home.controller.js
@@ -1,7 +1,10 @@
+import { response } from "express";
+
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+	"../model/formatter",
+	"sap/m/MessageBox"
+], function(Controller, formatter, MessageBox) {
 	"use strict";
 
 	return Controller.extend("jsg.fixpo.controller.App", {
@@ -10,6 +13,56 @@ sap.ui.define([
 
 		onInit: function () {
 
+		},
+
+		getRequestPromise: function(url, data) {
+			return new Promise((resolve, reject) => {
+				jQuery.ajax({
+					url: url,
+					type: 'GET',
+					success: data => resolve(data),
+					error: e => reject(e)
+				});
+			});
+		},
+
+		showError: function(text) {
+			return new Promise( resolve => {
+				MessageBox.show(
+					text, {
+						icon: MessageBox.Icon.ERROR,
+						title: 'Erro',
+						//actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+						onClose: () => resolve()
+					}
+				);
+			});
+		},
+
+		execGetRequest: async function(url, data) {
+			let result;
+
+			try {
+				this.getView().setBusy(true);
+				result = await this.getRequestPromise(url);
+			} catch (error) {
+				console.log(error);
+				this.showError(JSON.stringify(error));
+			} finally {
+				this.getView().setBusy(false);
+			}
+
+			return result;
+		},
+
+		onStart: async function(oEvent) {
+			let result = await this.execGetRequest('/daemon/exec/start');
+			sap.m.MessageToast.show(result);
+		},
+
+		onStop: async function(oEvent) {
+			let result = await this.execGetRequest('/daemon/exec/stop');
+			sap.m.MessageToast.show(result);
 		}
 	});
 });
