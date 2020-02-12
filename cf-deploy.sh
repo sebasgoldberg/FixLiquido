cc7afafd2c44f6cbde79014213dbf6fbaefff7fa
Sebastian Goldberg <sebas.goldberg@gmail.com>
2020-02-12 19:41:44 +0000

    Adição da criação do serviço application-logs no deploy.

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -1,5 +1,6 @@
 cf create-service xsuaa application uaa_FixLiquido -c xs-security.json 
 cf create-service destination lite dest_FixLiquido
+cf create-service application-logs lite logsFixLiquido
 cd daemon
 cf push
 cd ../router
